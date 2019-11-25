ff375fe22585ac1ec94b9fba4ca50e409ee838bd
Jonas Jacobsen <jonas.jacobsen1992@hotmail.de>
2019-11-25 20:47:31 +0100

    footer überarbeiet

--- components/footer.php
+++ components/footer.php
@@ -1,12 +1,49 @@
-<div class="footer">
+<div id="footer">
     <div class="container">
-        <?php
-        $jahr = date("Y", time());
-        echo "<p>&copy; XXL Baltic Yachting " . $jahr . " | <a href='impressum.php'>Impressum</a></p>";
-        ?>
+        <div class="titelschrift">
+            <a style="text-decoration: none; color: #FFFFFF" href="index.php">XXL Baltic Yachting</a>
+        </div>
+        <hr/>
+        <div class="row">
+            <div class="12u">
+                <section class="contact">
+                    <div class="texte">
+                        <header>
+                            <h3>Findet uns auch auf Facebook</h3>
+                        </header>
+                        <p>Ihr habe gute Erfahrungen mit uns gemacht? Dann kommt uns doch auch bei <a href="#" target="_blank">Facebook</a> besuchen und klickt auf "gef&auml;llt mir".</p>
+                        <ul class="icons">
+                            <li><a href="#"
+                                   class="icon fa-facebook" target="_blank"><span class="label"><img class="opacity"
+                                                                                                     src="assets/icons/face.svg"
+                                                                                                     height="50em"
+                                                                                                     alt="face"></span></a>
+                                <a href="#"
+                                   class="icon in-instagram" target="_blank"><span class="label"><img class="opacity"
+                                                                                                      src="assets/icons/insta.svg"
+                                                                                                      height="50em"
+                                                                                                      alt="insta"></span></a>
+                                <a href="#" class="icon go-google"
+                                   target="_blank"><span class="label"><img class="opacity" src="assets/icons/google.svg"
+                                                                            height="50em" alt="google"></span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                <!-- Copyright -->
+                <div class="copyright">
+                    <ul class="menu">
+                        <li>&copy; XXL Baltic Yachting - <?php  $jahr = date("Y", time()); echo $jahr ?>  . All rights reserved. |</li>
+                        <li><a href="impressum.php">Impressum |</a></li>
+                        <li><a href="#">Sitemap</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 </body>
 </html>
+
